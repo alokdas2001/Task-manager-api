760f9280ff9ae7662dc51457d11dc5ad18f43763
alokdas2001 <57097702+alokdas2001@users.noreply.github.com>
2020-09-13 12:10:46 +0530

    Update account.js

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,6 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY); 
+sgMail.setApiKey(SENDGRID_API_KEY); 
 
 // sending welcome Email
 const sendWelcomeEmail = (email , name) =>{
